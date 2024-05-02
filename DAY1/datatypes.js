efee9cf7d7b05bb692ac3a00efdd1c560d0eae49
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-05-02 14:12:54 +0100

    feat: Day1 exercise completed

---
+++ DAY1/datatypes.js
@@ -0,0 +1,7 @@
+//task 5
+
+console.log(typeof 'Asabeneh')
+console.log(typeof false)
+console.log(typeof undefined)
+console.log(typeof null)
+
---
+++ DAY1/variable.js
@@ -0,0 +1,37 @@
+//task 4
+"Asabeneh"
+false
+let bobName
+console.log(bobName)
+ let emptySet = null
+
+
+
+//task 6
+let middleAge 
+let isPopular
+let positionInFamily
+let eldestChild
+
+//task 7
+let middleManCount = 7
+let numberInLibrary = 17
+let isOdd = false
+let customersNames = 'Mr Latin'
+
+//task8
+let firstName = 'Aishat'
+let lastName = 'Salawudeen'
+let isMarried = false
+let country = 'Nigeria'
+let age = 24
+
+//task 9
+let myFirstName = 'Aishat', myLastName = 'Salawudeen', notMarried = true, countryName = 'Nigerian', mAge = 24
+
+//task 10
+let myAge = "I am 24 years old"
+let yourAge = "You are 30 years old"
+
+console.log(myAge)
+console.log(yourAge)
