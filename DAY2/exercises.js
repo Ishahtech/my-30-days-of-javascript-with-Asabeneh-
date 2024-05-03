8545937618e4e04849eda736cfc26cdd9143c2b3
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-05-03 23:24:16 +0100

    fix:Day3, slowly but surely

---
+++ DAY2/exercises.js
@@ -0,0 +1,15 @@
+let challenge = '30 Days Of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase)
+console.log(challenge.toLowerCase)
+console.log(challenge.substring(0, 2))
+console.log(challenge.substring(0, 10))
+console.log(challenge.includes('Script'))
+console.log(challenge.split())
+console.log(challenge.split(' '))
+let string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(string.split(' ,'))
+console.log(challenge.replace('Javascript', 'Python' ))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt(J))
