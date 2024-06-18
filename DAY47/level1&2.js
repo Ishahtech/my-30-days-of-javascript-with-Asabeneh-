bed67d1c311b8b0a96f290047df9c57d4211b913
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-06-18 03:48:59 +0100

    DAY48

--- DAY47/level1&2.js
+++ DAY47/level1&2.js
@@ -1,8 +1,15 @@
 // Level 1
-// Store you first name, last name, age, country, city in your browser localStorage.$
+
+// Store you first name, last name, age, country, 
+// city in your browser localStorage.$
+
+
 // Level 2
+
+
 // Create a student object. The student object will have first name, 
-// last name, age, skills, country, enrolled keys and values for the keys. 
+// last name, age, skills, country, enrolled keys
+// and values for the keys. 
 // Store the student object in your browser localStorage.
 
 const student = {
