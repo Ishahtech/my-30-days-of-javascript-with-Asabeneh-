a48a952a9d8a0bad59b875103acad39b9adf5ab3
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-06-18 04:03:27 +0100

    DAY48

--- DAY48/level1&2.js
+++ DAY48/level1&2.js
@@ -3,22 +3,21 @@
 // Store you first name, last name, age, country, 
 // city in your browser localStorage.$
 
+const student = {
+  firstName: 'Asabeneh',
+  lastName:'Salawudeen',
+  age: 20,
+  skills: ['HTML', 'CSS', 'JS', 'React', 'Tailwind'],
+  country: 'Namibia'
+}
 
 // Level 2
-
 
 // Create a student object. The student object will have first name, 
 // last name, age, skills, country, enrolled keys
 // and values for the keys. 
 // Store the student object in your browser localStorage.
 
-const student = {
-    firstName: 'Asabeneh',
-    lastName:'Salawudeen',
-    age: 20,
-    skills: ['HTML', 'CSS', 'JS', 'React', 'Tailwind'],
-    country: 'Namibia'
-  }
   
   const studentText = JSON.stringify(student, undefined, 4)
   localStorage.setItem('student', studentText)
